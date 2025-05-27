712762c4589bda98e1990b28055d6f074444979f
david <david@elpuig.xeill.net>
2025-05-27 12:32:03 +0200

    PArticulas

---
+++ core/src/main/java/com/activeminds/mach1r/ParticleSystem.java
@@ -0,0 +1,29 @@
+package com.activeminds.mach1r;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector3;
+
+public class ParticleSystem {
+
+    class Particle {
+
+        Vector3 position, speed;
+        Texture sprite;
+
+
+        Particle(Vector3 position, Texture spr)
+        {
+
+        }
+    }
+
+    ParticleSystem()
+    {
+
+    }
+
+    void update()
+    {
+
+    }
+}
